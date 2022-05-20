e21a930779b80b0f06d090a2b8a1546f2e618272
"Lance" <"lance.a.585@gmail.com">
2022-05-19 19:35:28 -0700

    simplestorage

--- contract.sol
+++ contract.sol
@@ -1,1 +1,13 @@
-pragma 
+pragma solidity >=0.4.16 <0.9.0;
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
